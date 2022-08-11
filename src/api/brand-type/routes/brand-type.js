1070421a5d639d107e76c5d9d85914a1c968d5d6
root <root@ip-172-31-28-101.af-south-1.compute.internal>
2022-08-11 11:27:36 +0000

    latest changes

---
+++ src/api/brand-type/routes/brand-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * brand-type router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::brand-type.brand-type');
---
+++ src/api/brand-type/services/brand-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * brand-type service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::brand-type.brand-type');
