b2c751a4bd73abd087a22f955ddefb5eb4c45aca
Ubuntu <ubuntu@ip-172-31-28-174.af-south-1.compute.internal>
2023-02-05 10:34:45 +0000

    new changes

---
+++ src/api/holiday-type/routes/holiday-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * holiday-type router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::holiday-type.holiday-type');
---
+++ src/api/holiday-type/services/holiday-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * holiday-type service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::holiday-type.holiday-type');
